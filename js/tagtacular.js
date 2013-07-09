e1f2780f0850e114f9383756143eb8a4134dc491
burnsbert <eric.burns@dunnwell.com>
2013-07-09 10:02:00 -0400

    0.8.5 and later versions will be licensed under the MIT License, the same open source license jQuery and jQueryUI use

--- js/tagtacular.js
+++ js/tagtacular.js
@@ -1,23 +1,35 @@
 /* ===================================================
- * tagtacular.js v0.8.4
+ * tagtacular.js v0.8.5
  * A jQuery plugin for tags management.
  *
  * http://gototech.com/tagtacular
  * Docs: https://github.com/burnsbert/tagtacular/wiki
  * ===================================================
- * Copyright 2013 Eric W. Burns
+ * This software is released under the MIT License.
  *
- * Licensed under the Mozilla Public License, Version 2.0 You may not use this work except in compliance with the License.
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2013 Eric Burns
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  *
- * http://www.mozilla.org/MPL/2.0/
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * See online documentation for complete instructions. Requires jquery.js and jqueryui.js.
+ * See https://gototech.com/tagtacular for complete instructions. Requires jquery.js and jqueryui.js.
  * =================================================== */
 
 ;(function($){
